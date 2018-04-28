4e7643b0d2aa7a36be7823436daa51e03df99e8b
Justin Tamblyn <just1689@gmail.com>
2018-04-29 01:29:58 +0200

    Refactor

--- controller/incoming.go
+++ controller/incoming.go
@@ -14,59 +14,63 @@ func HandleIncoming(b []byte) {
 	go func(b []byte) {
 
 		jsonParsed, err := gabs.ParseJSON(b)
+		if err != nil {
+			log.Errorln(fmt.Sprintf("Error: %e", err))
+			return
+		}
 
 		con, _ := jsonParsed.Path("conversation").Data().(string)
-
-		if err != nil {
-			return
-		}
-		if con == messages.ConversationShareTag {
-			handleMyTagMessage(b)
-			return
-		} else if con == messages.ConversationListOfGames {
-			handleListOfGames(b)
-			return
-		} else if con == messages.ConversationShareDynamicThings {
-			handleDynamicThings(b)
-			return
-		} else if con == messages.ConversationShareMap {
-			handleMap(b)
-			return
-		} else if con == "S_SHARE_HEALTH" {
-			//Ignore
-			return
-		} else if con == "S_CHANGE_VIEW" {
-			//Ignore
-			return
-		} else if con == "S_SCOREBOARD" {
-			//Ignore
-			return
-		} else if con == "S_P_LEVEL" {
-			//Ignore
-			return
-		} else if con == "S_PLAY_SOUND" {
-			//Ignore
-			return
-		} else if con == "S_SHARE_BULLETS" {
-			//Ignore
-			return
-		} else if con == "S_SHARE_SPRAY" {
-			//Ignore
-			return
-		} else if con == "S_ORB_N" {
-			//Ignore
-			return
-		} else if con == "S_PLAYER_LEFT" {
-			//Ignore
-			return
-
-		} else if con == "" && string(b) == "0" {
-			util.SendMessage("1")
-			return
-		}
-
-		log.Infoln(fmt.Sprintf("Received: %s", con))
+		routeMessage(con, b)
 
 	}(b)
 
+}
+
+func routeMessage(con string, b []byte) {
+	if con == messages.ConversationShareTag {
+		handleMyTagMessage(b)
+		return
+	} else if con == messages.ConversationListOfGames {
+		handleListOfGames(b)
+		return
+	} else if con == messages.ConversationShareDynamicThings {
+		handleDynamicThings(b)
+		return
+	} else if con == messages.ConversationShareMap {
+		handleMap(b)
+		return
+	} else if con == "S_SHARE_HEALTH" {
+		//Ignore
+		return
+	} else if con == "S_CHANGE_VIEW" {
+		//Ignore
+		return
+	} else if con == "S_SCOREBOARD" {
+		//Ignore
+		return
+	} else if con == "S_P_LEVEL" {
+		//Ignore
+		return
+	} else if con == "S_PLAY_SOUND" {
+		//Ignore
+		return
+	} else if con == "S_SHARE_BULLETS" {
+		//Ignore
+		return
+	} else if con == "S_SHARE_SPRAY" {
+		//Ignore
+		return
+	} else if con == "S_ORB_N" {
+		//Ignore
+		return
+	} else if con == "S_PLAYER_LEFT" {
+		//Ignore
+		return
+
+	} else if con == "" && string(b) == "0" {
+		util.SendMessage("1")
+		return
+	}
+
+	log.Infoln(fmt.Sprintf("Received: %s", con))
 }
