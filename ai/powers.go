2873247595cd43fbc2e7cfe3b14ec18e363b923f
Justin Tamblyn <just1689@gmail.com>
2018-05-08 07:39:22 +0200

    Clean up

--- ai/powers.go
+++ ai/powers.go
@@ -2,11 +2,10 @@ package ai
 
 import (
 	"github.com/just1689/gg-bot-captain/controller"
-	log "github.com/sirupsen/logrus"
+	"github.com/sirupsen/logrus"
 )
 
 func shoot() {
-	//Todo: impl
 	controller.PressNumber1()
-	log.Infoln("Pew!")
+	logrus.Infoln("Pew!")
 }
