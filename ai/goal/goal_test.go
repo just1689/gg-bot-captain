f89ad06ae17969d5b4f3acf0ca3c67bb915b9da3
Justin Tamblyn <just1689@gmail.com>
2018-06-05 13:56:49 +0200

    Test goal creation

---
+++ ai/goal/goal_test.go
@@ -0,0 +1,34 @@
+package goal
+
+import (
+	"testing"
+	"github.com/sirupsen/logrus"
+	"fmt"
+	"github.com/magiconair/properties/assert"
+)
+
+func TestNewHideGoal(t *testing.T) {
+	name := "ai.goal.TestNewHideGoal"
+
+	myGoal := NewHideGoal()
+	assert.Equal(t, myGoal, Hide, "A hide goal should equate to hide")
+
+	if t.Failed() {
+		logrus.Println(fmt.Sprintf("Testing %s failed ❌ ", name))
+	}
+}
+
+func TestNewKillGoal(t *testing.T) {
+	name := "ai.goal.TestNewKillGoal"
+
+	var someTag uint
+	someTag = 4
+
+	myGoal := NewKillGoal(someTag)
+	assert.Equal(t, myGoal, Hide, "A kill goal should equate to kill")
+	assert.Equal(t, myGoal.Tag, someTag, "A target tag should be the target")
+
+	if t.Failed() {
+		logrus.Println(fmt.Sprintf("Testing %s failed ❌ ", name))
+	}
+}
