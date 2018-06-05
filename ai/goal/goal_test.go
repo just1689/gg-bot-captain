8e2424019fa0d3fec03d0c94619daefae4be9d34
Justin Tamblyn <just1689@gmail.com>
2018-06-05 19:46:31 +0200

    Bug fixes

--- ai/goal/goal_test.go
+++ ai/goal/goal_test.go
@@ -2,16 +2,16 @@ package goal
 
 import (
 	"fmt"
-	"github.com/magiconair/properties/assert"
 	"github.com/sirupsen/logrus"
 	"testing"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestNewHideGoal(t *testing.T) {
 	name := "ai.goal.TestNewHideGoal"
 
 	myGoal := NewHideGoal()
-	assert.Equal(t, myGoal, Hide, "A hide goal should equate to hide")
+	assert.Equal(t, myGoal.Goal, Hide, "A hide goal should equate to hide")
 
 	if t.Failed() {
 		logrus.Println(fmt.Sprintf("Testing %s failed ❌ ", name))
@@ -25,7 +25,7 @@ func TestNewKillGoal(t *testing.T) {
 	someTag = 4
 
 	myGoal := NewKillGoal(someTag)
-	assert.Equal(t, myGoal, Hide, "A kill goal should equate to kill")
+	assert.Equal(t, myGoal.Goal, Kill, "A kill goal should equate to kill")
 	assert.Equal(t, myGoal.Tag, someTag, "A target tag should be the target")
 
 	if t.Failed() {
