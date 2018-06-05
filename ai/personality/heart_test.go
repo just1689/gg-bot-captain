bb63b75256331f15f0e28bcbcddc66d285f7c37b
Justin Tamblyn <just1689@gmail.com>
2018-06-05 09:02:27 +0200

    Test the personality

---
+++ ai/personality/heart_test.go
@@ -0,0 +1,45 @@
+package personality
+
+import (
+	"fmt"
+	"github.com/just1689/gg-bot-captain/mem"
+	"github.com/just1689/gg-bot-captain/model"
+	"github.com/sirupsen/logrus"
+	"github.com/stretchr/testify/assert"
+	"testing"
+	"github.com/just1689/gg-bot-captain/ai/goal"
+)
+
+const NAME = "ai.PickTarget"
+
+func TestChooseGoal(t *testing.T) {
+
+	//Setup the game
+	mem.Init()
+	myTag := uint(1)
+	me := model.Tag{Tag: myTag}
+	mem.Push(model.TableNameTag, me)
+	myTank := model.Thing{Tag: me.Tag}
+	mem.Push(model.TableNameThing, myTank)
+
+	myPersonality := Hunter
+	myGoal := ChooseGoal(myPersonality)
+	assert.Equal(t, myGoal.Goal, goal.Hide, "A hunter should hide when there is no other player")
+
+	//Add a player
+	theirTag := uint(2)
+	theirTank := model.Thing{Tag: theirTag}
+	mem.Push(model.TableNameThing, theirTank)
+
+	myGoal = ChooseGoal(myPersonality)
+	assert.Equal(t, myGoal.Goal, goal.Kill, "A hunter should kill if there is another player")
+	assert.Equal(t, myGoal.Tag, theirTag, "A hunter should kill the only other player")
+
+	myPersonality = Wimp
+	myGoal = ChooseGoal(myPersonality)
+	assert.Equal(t, myGoal.Goal, goal.Hide, "A wimp should hide")
+
+	if t.Failed() {
+		logrus.Println(fmt.Sprintf("Testing %s failed ❌ ", NAME))
+	}
+}
