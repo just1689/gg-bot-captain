ff0526677c21b623a61e3746aaa865b47e059db8
Captain Justin <just1689@gmail.com>
2018-06-07 19:04:42 +0200

    The scheduler should provider a ticker to signal that it is running. It should sleep between runs

--- ai/scheduler.go
+++ ai/scheduler.go
@@ -3,18 +3,23 @@ package ai
 import (
 	"github.com/just1689/gg-bot-captain/ai/personality"
 	"github.com/sirupsen/logrus"
+	"time"
 )
 
 //Schedule handles the running of the AI go routine
-func Schedule() {
-
+func Schedule() (ticker chan bool) {
+	ticker = make(chan bool)
 	go func() {
 		for {
 			logrus.Println("ai.Scheduler run...")
 			myPersonality := personality.ChoosePersonality()
+			ticker <- true
 			myGoal := personality.ChooseGoal(myPersonality)
+			ticker <- true
 			plan(myGoal, myPersonality)
+			ticker <- true
+			time.Sleep(1 * time.Second)
 		}
 	}()
-
+	return ticker
 }
