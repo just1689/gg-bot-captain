914b32cb2602307f82282bc71f71e26247c67c51
Justin Tamblyn <just1689@gmail.com>
2018-06-07 08:24:39 +0200

    Testing GetAllThings

--- controller/things_test.go
+++ controller/things_test.go
@@ -13,7 +13,7 @@ import (
 
 func TestPersistThingsMessage(t *testing.T) {
 
-	name := "controller.things_test.go"
+	name := "controller.TestPersistThingsMessage"
 
 	mem.Init()
 	myTag := uint(1)
@@ -36,5 +36,50 @@ func TestPersistThingsMessage(t *testing.T) {
 	if t.Failed() {
 		logrus.Println(fmt.Sprintf("Testing %s failed ❌ ", name))
 	}
+
+}
+
+func TestGetAllThings(t *testing.T) {
+
+	name := "controller.TestGetAllThings"
+	var tag uint
+	var max uint
+	var min uint
+
+	mem.Init()
+	min = 100
+	max = 200
+	totalInserted := 0
+	for tag = min; tag <= max; tag++ {
+		insertTankBlocking(tag)
+		totalInserted++
+	}
+
+	c := GetAllThings()
+
+	total := 0
+	for item := range c {
+		assert.True(t, item.Tag >= min || item.Tag <= max, "The tag should be one of those created")
+		total++
+	}
+	assert.Equal(t, totalInserted, total, "The number inserted should be equal to the number read")
+
+	if t.Failed() {
+		logrus.Println(fmt.Sprintf("Testing %s failed ❌ ", name))
+	}
+
+}
+
+func insertTankBlocking(tag uint) {
+	myTag := uint(tag)
+	me := model.Tag{Tag: myTag}
+	mem.Push(model.TableNameTag, me)
+	myTank := model.Thing{Tag: me.Tag}
+	msg := incoming.MessageShareDynamicThings{}
+	things := []model.Thing{myTank}
+	msg.Things = things
+	bytes, _ := json.Marshal(myTank)
+	signal := PersistThingsMessage(bytes)
+	<-signal
 
 }
