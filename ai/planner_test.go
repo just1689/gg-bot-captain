ff3c55c74ff53964356df81b0a06b2b33db331c8
Captain Justin <just1689@gmail.com>
2018-06-19 17:49:03 +0200

    The planner should send back a list of actions

--- ai/planner_test.go
+++ ai/planner_test.go
@@ -12,7 +12,7 @@ import (
 
 func TestPlan(t *testing.T) {
 
-	name := "ai.personality.TestChoosePersonality"
+	name := "ai.personality.TestPlan"
 
 	//Setup the game
 	mem.Init()
@@ -34,6 +34,37 @@ func TestPlan(t *testing.T) {
 	_, ok := plan(myGoal, myPersonality)
 
 	assert.True(t, ok, "The planner should return ok is true")
+
+	if t.Failed() {
+		logrus.Println(fmt.Sprintf("Testing %s failed ❌ ", name))
+	}
+
+}
+
+func TestPlanActions(t *testing.T) {
+
+	name := "ai.personality.TestPlanActions"
+
+	//Setup the game
+	mem.Init()
+
+	//Add my player
+	myTag := uint(1)
+	me := model.Tag{Tag: myTag}
+	mem.Push(model.TableNameTag, me)
+	myTank := model.Thing{Tag: me.Tag}
+	mem.Push(model.TableNameThing, myTank)
+
+	//Add a player
+	theirTag := uint(2)
+	theirTank := model.Thing{Tag: theirTag}
+	mem.Push(model.TableNameThing, theirTank)
+
+	myPersonality := personality.Hunter
+	myGoal := personality.ChooseGoal(myPersonality)
+	actions, _ := plan(myGoal, myPersonality)
+
+	assert.NotEmpty(t, actions, "The list of actions planned should not be empty")
 
 	if t.Failed() {
 		logrus.Println(fmt.Sprintf("Testing %s failed ❌ ", name))
