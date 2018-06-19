4752467b0aa40ff59a82952809f53ca92e8d0b21
Captain Justin <just1689@gmail.com>
2018-06-19 17:51:31 +0200

    An action has a label and a target tag

--- model/action.go
+++ model/action.go
@@ -1,4 +1,14 @@
 package model
 
+type ActionLabel int
+
+const (
+	ActionAim      ActionLabel = iota
+	ActionMoveNear
+	ActionAttack
+)
+
 type Action struct {
+	action    ActionLabel
+	tagTarget uint
 }
