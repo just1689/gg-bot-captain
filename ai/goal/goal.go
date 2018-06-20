efca05a46c30d0f436072cb3644a37d43068bc5f
Justin Tamblyn <just1689@gmail.com>
2018-06-20 11:17:19 +0200

    Goals can be satisfied

--- ai/goal/goal.go
+++ ai/goal/goal.go
@@ -19,3 +19,29 @@ func NewKillGoal(tag uint) Goal {
 	item := Goal{Goal: Kill, Tag: tag}
 	return item
 }
+
+func (g Goal) IsSatisfied() bool {
+	if g.Goal == Kill {
+		return g.isSatisfiedKill()
+	} else if g.Goal == Hide {
+		return g.isSatisfiedHide()
+	}
+
+	/*
+		Goals with no satisfaction criteria risk implied yet unintended endless execution
+		This means it would be better for it to consider it satisfied if does not explicitly
+		implement criteria to measure that.
+
+	 */
+	return true
+}
+
+func (g Goal) isSatisfiedKill() bool {
+	//Todo: time-series implementation for capturing state related to deaths
+	return false
+}
+
+func (g Goal) isSatisfiedHide() bool {
+	//Todo: is hiding ever complete?
+	return false
+}
