02ec3d43b0913c3d8d47ac70d531f8d1ca454aa3
Justin Tamblyn <just1689@gmail.com>
2018-06-20 13:54:48 +0200

    Refactor

--- ai/personality/heart_test.go
+++ ai/personality/heart_test.go
@@ -2,6 +2,7 @@ package personality
 
 import (
 	"fmt"
+	"github.com/just1689/gg-bot-captain/ai/planner"
 	"github.com/just1689/gg-bot-captain/mem"
 	"github.com/just1689/gg-bot-captain/model"
 	"github.com/sirupsen/logrus"
@@ -51,4 +52,41 @@ func TestChooseGoal(t *testing.T) {
 	if t.Failed() {
 		logrus.Println(fmt.Sprintf("Testing %s failed ❌ ", name))
 	}
+}
+
+func TestAct(t *testing.T) {
+
+	setupTwoPlayers()
+
+	//Test the hunters actions
+	myPersonality := model.Hunter
+	myGoal := personality.ChooseGoal(myPersonality)
+	myActions, ok := planner.Plan(myPersonality, myGoal)
+	assert.True(t, ok, "The planner should give the ok for TestAct")
+	Act(myPersonality, myGoal, myActions)
+
+	myPersonality = model.Wimp
+	myGoal = personality.ChooseGoal(myPersonality)
+	myActions, ok = planner.Plan(myPersonality, myGoal)
+	assert.True(t, ok, "The planner should give the ok for TestAct")
+	Act(myPersonality, myGoal, myActions)
+
+}
+
+func setupTwoPlayers() {
+	//Setup the game
+	mem.Init()
+
+	//Add my player
+	myTag := uint(1)
+	me := model.Tag{Tag: myTag}
+	mem.Push(model.TableNameTag, me)
+	myTank := model.Thing{Tag: me.Tag}
+	mem.Push(model.TableNameThing, myTank)
+
+	//Add a player
+	theirTag := uint(2)
+	theirTank := model.Thing{Tag: theirTag}
+	mem.Push(model.TableNameThing, theirTank)
+
 }
