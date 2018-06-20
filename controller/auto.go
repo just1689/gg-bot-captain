b59fe7b3b4a43f19c2e092a75fcde570eead5e6d
Captain Justin <just1689@gmail.com>
2018-06-20 18:33:34 +0200

    Stubbed out aim

--- controller/auto.go
+++ controller/auto.go
@@ -1,11 +1,21 @@
 package controller
 
+import "github.com/just1689/gg-bot-captain/model"
+
 func autoMoveNear(tag uint) {
 	//Todo: implement
 }
 
-func autoAim(tag uint) {
+func autoAim(targetTag uint) {
+	me, target := GetMeAndTarget(GetMyTag(), targetTag)
 	//Todo: implement
+	r := model.GetRotationBetween(me, target)
+	if r > 0 {
+		//Rotate left
+	} else {
+		//Rotate right
+	}
+
 }
 
 func autoAttack(tag uint) {
