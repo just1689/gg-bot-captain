2165151b2605c5085d44018c7cbd976d9473a8a3
Justin Tamblyn <just1689@gmail.com>
2018-06-20 11:28:08 +0200

    Use personality and check action to execute

--- controller/actor.go
+++ controller/actor.go
@@ -6,13 +6,26 @@ import (
 )
 
 func Act(myPersonality model.Personality, goal model.Goal, actions []model.Action) {
+
 	for _, action := range actions {
+
+		if myPersonality.IsNowIrrelevant() {
+			log.Println("My personality doesn't fit. Aborting")
+			return
+		}
+
 		if goal.IsSatisfied() {
 			log.Println("The goal has been satisfied. Aborting")
 			return
 		}
 
 		if action.Action == model.ActionMoveNear {
+
+		} else if action.Action == model.ActionAim {
+
+		} else if action.Action == model.ActionAttack {
+
+		} else if action.Action == model.ActionRunAway {
 
 		}
 
